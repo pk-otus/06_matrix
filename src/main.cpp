4e05be5c7e3cc97725e26aec6738633edb0c3f2d
Paul <a@a.a>
2018-08-17 21:58:43 +0300

    +

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 
-#include <matrixFunctions.h>
+#include "../include/matrixFunctions.h"
 
 int main(int, char *[])
 {
--- test/test_main.cpp
+++ test/test_main.cpp
@@ -1,6 +1,6 @@
 #include <gtest/gtest.h>
 
-#include <matrixFunctions.h>
+#include "../include/matrixFunctions.h"
 
 TEST(MatrixTest, MatrixTest)
 {
